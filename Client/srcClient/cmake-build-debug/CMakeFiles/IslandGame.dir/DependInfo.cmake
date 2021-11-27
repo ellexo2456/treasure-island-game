35ba2928b4989dd1a71d33712d3f3a8f729f22cb
Sergey <sereja.gal@yandex.ru>
2021-11-27 13:46:41 +0300

    First_commit

--- Client/srcClient/cmake-build-debug/CMakeFiles/IslandGame.dir/DependInfo.cmake
+++ Client/srcClient/cmake-build-debug/CMakeFiles/IslandGame.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ser/_1_treasure_island_game/srcClient/main.cpp" "/home/ser/_1_treasure_island_game/srcClient/cmake-build-debug/CMakeFiles/IslandGame.dir/main.cpp.o"
+  "/home/ser/_1_treasure_island_game/Client/srcClient/main.cpp" "/home/ser/_1_treasure_island_game/Client/srcClient/cmake-build-debug/CMakeFiles/IslandGame.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
